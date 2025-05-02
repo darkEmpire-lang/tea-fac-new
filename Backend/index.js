cc124fe26483c0df0732d88913cb1d0debb758b4
darkEmpire-lang <it22546784@my.sliit.lk>
2025-05-02 08:12:19 +0530

    add vercel json file

--- Backend/index.js
+++ Backend/index.js
@@ -18,31 +18,30 @@ import budgetRoutes from './routes/budgetRoutes.js';
 const app = express();
 
 const allowedOrigins = [
-  'https://e-commerce-frontend-one-rouge.vercel.app', 
-  'https://deal-mate-admin-panel.vercel.app',
-  'https://e-commerce-frontend-y03qtvm4g-pasindus-projects-e32111c9.vercel.app',
-   'https://deal-mates-kohl.vercel.app'
+  'https://newlands-tea-factory-bcjthgnry-pasindus-projects-e32111c9.vercel.app',
+  'localhost:5173',  
+  
 ];
 
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-// // CORS middleware configuration
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // Allow requests with no origin (e.g., mobile apps, CURL)
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error('The CORS policy for this site does not allow access from the specified origin.'));
-//       }
-//     }
-//   })
-// );
+// CORS middleware configuration
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (e.g., mobile apps, CURL)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('The CORS policy for this site does not allow access from the specified origin.'));
+      }
+    }
+  })
+);
 
 
 
